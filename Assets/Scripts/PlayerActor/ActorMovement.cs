ac8d3545f92d35919c551b7394a74e783c687624
sanyagvozdoder <alexshadrov1712@gmail.com>
2024-06-04 15:36:47 +0700

    ctrl

--- Assets/Scripts/PlayerActor/ActorMovement.cs
+++ Assets/Scripts/PlayerActor/ActorMovement.cs
@@ -12,11 +12,16 @@ namespace Actor
     {
         [SerializeField] private InputAction _movement;
         [SerializeField] private InputAction _jump;
+        [SerializeField] private InputAction _crouch;
         [SerializeField, Range(0, 1)] private float _accelCoefficient = 0.5f;
         [SerializeField, Range(0, 1)] private float _frictionCoefficient = 0.2f;
         [SerializeField] private float _movementSpeed = 5;
-        [SerializeField] private float _airMovementSpeed = 3;
+        [SerializeField] private float _airMovementSpeed = 3.5f;
+        [SerializeField] private float _crouchMovementSpeed = 3.5f;
         [SerializeField] private float _jumpForce = 500;
+        [SerializeField] private float _crouchHeight = 1.0f;
+
+        private float _originalHeight;
 
         [SerializeField] private float _groundCheckRadius = 0.35f;
         [SerializeField] private float _groundCheckHeight = 0.81f;
@@ -26,6 +31,7 @@ namespace Actor
         private CapsuleCollider _collider;
 
         private bool _onGround;
+        private bool _onCrouch = false;
 
         private Vector3 Forward => transform.forward;
         private Vector3 Right => transform.right;
@@ -41,11 +47,13 @@ namespace Actor
 
             _collider = GetComponent<CapsuleCollider>();
             _collider.material = new PhysicMaterial() { staticFriction = 1, dynamicFriction = 0 };
+            _originalHeight = _collider.height;
 
             _movement.Enable();
 
             SessionProvider.Current.SetActor(gameObject);
             _jump.Enable();
+            _crouch.Enable();
         }
 
         private void Update()
@@ -57,13 +65,41 @@ namespace Actor
                 var force = _jumpForce * Vector3.up;
                 _rigidbody.AddForce(force, ForceMode.Impulse);
             }
+            if (_crouch.IsPressed())
+            {
+                _onCrouch = true;
+                _collider.height = _crouchHeight;
+            }
+            else
+            {
+                _onCrouch = false;
+                _collider.height = _originalHeight;
+            }
         }
 
         private void FixedUpdate()
         {
             var input = _movement.ReadValue<Vector2>();
 
-            if (_onGround)
+            if(_onCrouch)
+            {
+                if (input.sqrMagnitude > 0)
+                {
+                    var velocity = _rigidbody.velocity;
+                    var desiredVelocity = (Forward * input.y + Right * input.x) * _crouchMovementSpeed;
+                    var force = desiredVelocity - velocity;
+                    force.y = 0;
+
+                    _rigidbody.AddForce(force * _accelCoefficient, ForceMode.Impulse);
+                }
+                else
+                {
+                    var force = -_rigidbody.velocity * _frictionCoefficient;
+                    force.y = 0;
+
+                }
+            }
+            else if (_onGround)
             {
                 if (input.sqrMagnitude > 0)
                 {
@@ -95,7 +131,6 @@ namespace Actor
                     var force = -_rigidbody.velocity * _frictionCoefficient;
                     force.y = 0;
 
-                    //_rigidbody.AddForce(force, ForceMode.Impulse);
                 }
             }
         }
